531277f1ced8460f95d34acc40a3d72b29681cd0
Camille Xue <the.camille.xue@gmail.com>
2025-05-02 12:43:42 -0700

    testing with earned gold

--- Assets/Fountain/CoinController.lua
+++ Assets/Fountain/CoinController.lua
@@ -19,20 +19,22 @@ local throwTween = Tween:new(
     TweenModule.Easing.easeOutBack,
     function(value, t)
         positionOffset = positionCurve:Evaluate(value)
+
         self.gameObject.transform.position = Vector3.Lerp(startPos, targetPos, value) + Vector3.new(0, positionOffset*2, 0)
         self.gameObject.transform.eulerAngles = Vector3.new(value*angularSpeed, 0, value*(angularSpeed/2))
     end,
     function()
-        GameObject.Destroy(self.gameObject)
+        
     end
 )
 
 function self:Awake()
-    startPos = self.gameObject.transform.position + Vector3.new(0,2,0)
+    startPos = self.gameObject.transform.position
 
 end
 
 function throw(targetPosition)
     targetPos = targetPosition
     throwTween:start()
+    Timer.After(.6, function() GameObject.Destroy(self.gameObject) throwTween:stop() end)
 end
