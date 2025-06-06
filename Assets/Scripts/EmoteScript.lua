8927ec7cc8419be98765091831dc33f3f275f05b
annmanly <ann@high.rs>
2025-06-06 18:56:03 -0400

    small updates to indicators and links, signs

--- Assets/Scripts/EmoteScript.lua
+++ Assets/Scripts/EmoteScript.lua
@@ -5,28 +5,33 @@ local emote_ids : { string } = {} -- List of emote IDs.
 --!SerializeField
 local is_loop : boolean = false -- Should the emotes loop?
 
+local character = nil
+
 function self:Awake()
-    local character = self.gameObject:GetComponent(Character)
+    character = self.gameObject:GetComponent(Character)
     if not character then
         print("No Character component found on " .. self.gameObject.name)
         return
     end
 
-    local function playEmote()
-        if #emote_ids == 0 then
-            print("No emotes to play on " .. self.gameObject.name)
-            return
-        end
+    playEmote()
+end
 
-        -- Pick a random emote and play it.
-        local emoteIndex = math.random(1, #emote_ids)
-        character:PlayEmote(emote_ids[emoteIndex], false, function()
-            -- If it should loop, play another emote after this one.
-            if is_loop then
-                playEmote() -- Calls itself to start another emote.
-            end
-        end)
+function self:Update()
+    if character == nil or not is_loop then return end
+
+    if character.state == CharacterState.Idle then
+        playEmote()
+    end
+end
+
+function playEmote()
+    if #emote_ids == 0 then
+        print("No emotes to play on " .. self.gameObject.name)
+        return
     end
 
-    playEmote()
+    -- Pick a random emote and play it.
+    local emoteIndex = math.random(1, #emote_ids)
+    character:PlayEmote(emote_ids[emoteIndex], false)
 end
